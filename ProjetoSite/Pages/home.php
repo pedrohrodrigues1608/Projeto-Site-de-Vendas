c93c2875d18a6bed92e11d7e1d4264efbdeefa05
Pedro Rodrigues <167270136+pedrohrodrigues1608@users.noreply.github.com>
2024-12-03 01:58:57 -0300

    Criei a NavBar
    Criei o primeiro protótipo da NavBar

---
+++ ProjetoSite/Pages/home.php
@@ -0,0 +1,49 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MercaStyle</title>
+    <link rel="stylesheet" href="./../Style/geralStyle.css">
+</head>
+<body>
+    <div id="navBar"> 
+
+        <div id="logo">
+            <img src="./../Images/Logo.png" href="">
+        </div>
+        <div id="menu">
+            <a id="btnInicio" href="">Início</a>
+            <a id="btnSobreNos" href="">Sobre nós</a>
+            <a id="btnContato" href="">Contato</a>
+            <a id="btnAjuda" href="">Ajuda</a>
+        </div>
+
+        <div id="loginECadastro">
+            <a id="btnEntrar" href="">Entrar na conta</a>
+            <a id="btnCadastrar" href="">Cadastrar</a>
+        </div>
+
+        <div>
+            <img  id="carrinho" src="./../Images/Carrinho.png" href="">
+        </div>
+
+        <div id="perfil">
+            <img id="iconPerfil" src="./../Images/Profile.png" href="">
+            <img id="circuloPerfil" src="./../Images/CirculoPerfil.png" href="">
+        </div>
+
+        
+    </div>
+    
+    <div id="content">
+        
+    </div>
+</body>
+</html>
